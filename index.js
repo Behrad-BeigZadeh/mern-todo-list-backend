a6af023aec6775e99d63da22c44b5b15a4aeccd4
behrad <berad.gmail.com>
2025-02-03 03:43:13 -0500

    file changed

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ const PORT = 5000 || process.env.PORT;
 app.use(express.json());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: [process.env.FRONTEND_URL],
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
-    optionSuccessStatus: 200,
   })
 );
 
